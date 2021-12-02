7cd61f38b3c5c6d71877b98260c0d039e0aac88e
sdwdjzhy <sdwdjzhy@qq.com>
2021-12-02 23:01:26 +0800

    创建一个解析搜索信息的生成器

---
+++ run-server/RunUI.AspNetCore/Models/QueryWrapper.cs
@@ -0,0 +1,233 @@
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+
+    public class QueryGenertator<T> where T : class, new()
+    {
+        Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private void SetProp(WhereHelper<T> whereHelper, string propertyName, string value, Type structType, string type)
+        {
+            if (value.IsNullOrWhiteSpace()) return;
+            try
+            {
+                object v = Convert.ChangeType(value, structType);
+                switch (type)
+                {
+                    case ">":
+                        whereHelper.GreaterThan(propertyName, v);
+                        break;
+
+                    case "<":
+                        whereHelper.LessThan(propertyName, v);
+                        break;
+
+                    case ">=":
+                        whereHelper.GreaterThanOrEqual(propertyName, v);
+                        break;
+
+                    case "<=":
+                        whereHelper.LessThanOrEqual(propertyName, v);
+                        break;
+
+                    case "==":
+                        whereHelper.Equal(propertyName, v);
+                        break;
+
+                    case "!=":
+                        whereHelper.NotEqual(propertyName, v);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                var msg = $"对于属性【{propertyName}】, 值【{value}】不能转换为[{structType.Name}]";
+
+                Logger.Error(ex, msg);
+            }
+        }
+        public Expression<Func<T, bool>> GetExpression(Stream stream)
+        {
+            JsonSerializer serializer = new();
+            using (StreamReader sr = new(stream))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                var obj = serializer.Deserialize(reader) as JObject;
+
+                JArray where = obj["where"] as JArray;
+
+                WhereHelper<T> whereHelper = new();
+                var modelType = typeof(T);
+                var props = modelType.GetProperties();
+                where.ForEach(x =>
+                {
+                    var item = JObject.FromObject(x);
+                    var column = item["column"].ToString();
+                    if (column.IsNullOrWhiteSpace())
+                    {
+                        throw new ArgumentNullException("column");
+                    }
+
+                    var p = props.FirstOrDefault(t => t.Name == column);
+                    if (p == null) return;
+                    var propertyName = p.Name;
+                    var value = item["value"] as JValue;
+                    if (p.PropertyType == typeof(string))
+                    {
+                        if (value == null) return;
+                        var s = value.Value.ToStringExt();
+                        if (s.HasValue())
+                        {
+                            if (s.StartsWith("^") || s.EndsWith("$"))
+                            {
+                                if (s.StartsWith("^") && s.EndsWith("$"))
+                                {
+                                    s = s[1..^1];
+                                    whereHelper.Equal(propertyName, s);
+                                }
+                                else if (s.StartsWith("^"))
+                                {
+                                    s = s[1..];
+                                    whereHelper.StartsWith(propertyName, s);
+                                }
+                                else if (s.EndsWith("$"))
+                                {
+                                    s = s[..^1];
+                                    whereHelper.EndsWith(propertyName, s);
+                                }
+                            }
+                            else if (s.StartsWith("=="))
+                            {
+                                s = s[2..];
+                                whereHelper.Equal(propertyName, s);
+                            }
+                            else if (s.StartsWith("!="))
+                            {
+                                s = s[2..];
+                                whereHelper.NotEqual(propertyName, s);
+                            }
+                            else
+                            {
+                                whereHelper.Contains(propertyName, s);
+                            }
+                        }
+                    }
+                    if (p.PropertyType == typeof(bool) || p.PropertyType == typeof(bool?))
+                    {
+                        if (value == null) return;
+                        var b = value.Value as bool?;
+                        if (b != null)
+                        {
+                            whereHelper.Equal(propertyName, b.Value);
+                        }
+                    }
+                    else if (p.PropertyType == typeof(Guid) || p.PropertyType == typeof(Guid?))
+                    {
+                        if (value == null) return;
+                        if (value.Value is Guid g && g != Guid.Empty)
+                            whereHelper.Equal(propertyName, g);
+                    }
+                    else if (p.PropertyType == typeof(DateTime) || p.PropertyType == typeof(DateTime?))
+                    {
+                        if (value != null && value.Value is DateTime g && g != DateTime.MinValue)
+                            whereHelper.Equal(propertyName, g);
+                        if (item["min"] is JValue val && val.Value is DateTime minDt && minDt != DateTime.MinValue)
+                            whereHelper.GreaterThanOrEqual(propertyName, minDt);
+                        if (item["max"] is JValue val2 && val2.Value is DateTime maxDt && maxDt != DateTime.MinValue)
+                            whereHelper.LessThanOrEqual(propertyName, maxDt);
+                    }
+                    else if (p.PropertyType.IsEnum || (p.PropertyType.IsNullableType() && p.PropertyType.GenericTypeArguments[0].IsEnum))
+                    {
+                        //枚举
+                        if (value != null && value.Value != null && value.Value is int @enum)
+                            whereHelper.Equal(propertyName, @enum);
+                        if (item["arr"] != null && item["arr"].Type == JTokenType.Array)
+                        {
+                            var list = item["arr"].ToString().JsonDeserialize<List<int>>();
+                            if (list.Any())
+                            {
+                                whereHelper.ArrayContains(propertyName, list);
+                            }
+                        }
+                    }
+                    else if (p.PropertyType.IsNumberType() || (p.PropertyType.IsNullableType() && p.PropertyType.GenericTypeArguments[0].IsNumberType()))
+                    {
+                        var structType = p.PropertyType;
+                        if (structType.IsNullableType())
+                        {
+                            structType = structType.GetGenericArguments()[0];
+                        }
+                        if (value.Value != null && value.Value is string str && str.HasValue())
+                        {
+                            var s = str;
+
+                            if (s.ContainsIgnoreCase("~") && !s.Trim().EqualsIgnoreCase("~"))
+                            {
+                                var ss = s.Split("~").Take(2).ToList();
+                                if (ss.Any())
+                                {
+                                    SetProp(whereHelper, propertyName, ss[0], structType, ">=");
+                                    SetProp(whereHelper, propertyName, ss[1], structType, "<=");
+                                }
+                            }
+                            else if (s.StartsWith(">="))
+                            {
+                                var n = s[2..];
+                                SetProp(whereHelper, propertyName, n, structType, ">=");
+                            }
+                            else if (s.StartsWith("<="))
+                            {
+                                var n = s[2..];
+                                SetProp(whereHelper, propertyName, n, structType, "<=");
+                            }
+                            else if (s.StartsWith(">"))
+                            {
+                                var n = s[1..];
+                                SetProp(whereHelper, propertyName, n, structType, ">");
+                            }
+                            else if (s.StartsWith("<"))
+                            {
+                                var n = s[1..];
+                                SetProp(whereHelper, propertyName, n, structType, "<");
+                            }
+                            else if (s.StartsWith("=="))
+                            {
+                                var n = s[2..];
+                                SetProp(whereHelper, propertyName, n, structType, "==");
+                            }
+                            else if (s.StartsWith("!="))
+                            {
+                                var n = s[2..];
+                                SetProp(whereHelper, propertyName, n, structType, "!=");
+                            }
+                            else if (s.Contains('，') || s.Contains(','))
+                            {
+                                var ss = s.Split逗号();
+                                if (ss.Any())
+                                {
+                                    var list = ss.Select(x => x.ToDoubleOrNull()).Where(i => i.HasValue).Select(x => x.Value).ToList();
+                                    whereHelper.ArrayContains(propertyName, list);
+                                }
+                            }
+                        }
+                    }
+                });
+
+                return whereHelper.GetExpression();
+            }
+
+
+        }
+    }
+}
---
+++ run-server/RunUI.AspNetCore/Models/WhereHelper.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+    /// <summary>
+    /// 构建Where条件
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class WhereHelper<T>
+        where T : class
+    {
+        public ParameterExpression Paramater { get; }
+        private readonly Type type;
+        private Expression filter;
+
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        public WhereHelper()
+        {
+            Paramater = Expression.Parameter(typeof(T), "c");
+            filter = null;
+            type = typeof(T);
+        }
+
+        public void And(Expression result)
+        {
+            filter = filter != null ? Expression.AndAlso(filter, result) : result;
+        }
+
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <typeparam name="K"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void ArrayContains<K>(string propertyName, ICollection<K> value)
+        {
+            var left = Expression.Constant(value);
+            var right = Expression.Property(Paramater, type.GetProperty(propertyName));
+            var result = Expression.Call(left, typeof(ICollection<K>).GetMethod("Contains"), right);
+            And(result);
+        }
+
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <param name="t"></param>
+        public void ArrayContains(string propertyName, object value, Type t)
+        {
+            var propertyInfo = type.GetProperty(propertyName);
+            var propertyType = propertyInfo.PropertyType;
+            if (t.GenericTypeArguments.Length > 0 && propertyType == t.GenericTypeArguments[0])//当类型相同优先走Contains
+            {
+                var left = Expression.Constant(value);
+                var right = Expression.Property(Paramater, type.GetProperty(propertyName));
+                var result = Expression.Call(left, t.GetMethod("Contains"), right);
+                And(result);
+            }
+            else
+            {
+                //var listType = typeof(List<>).MakeGenericType(propertyType);
+                //var left = Expression.Constant(value, listType);
+                Expression a = null;
+                {//先判断不为空
+                    var p = type.GetProperty(propertyName);
+                    var left = Expression.Property(Paramater, p);
+                    var right = Expression.Constant(null, p.PropertyType);
+                    var result = Expression.NotEqual(left, right);
+                    a = result;
+                    //And(result);
+                }
+                {//再Contains
+                    var left = Expression.Constant(value);
+                    var right = Expression.Property(Paramater, type.GetProperty(propertyName));
+                    right = Expression.Property(right, "Value");
+                    Expression result = Expression.Call(left, t.GetMethod("Contains"), right);
+                    result = Expression.AndAlso(a, result);
+                    And(result);
+                }
+
+
+                //if (value is System.Collections.IList values)//转化为
+                //{
+                //    var property = Expression.Property(Paramater, propertyInfo);
+                //    var right = Expression.Constant(values[0], propertyType);
+                //    Expression body = Expression.Equal(property, right);
+                //    for (var i = 1; i < values.Count; i++)
+                //    {
+                //        right = Expression.Constant(values[i], propertyType);
+                //        body = Expression.Or(body, Expression.Equal(property, right));
+                //    }
+                //    And(body);
+                //}
+                //else
+                //{
+                //    //未实现
+                //    throw new NotImplementedException("未实现类型不同且非IList的_Arr查询");
+                //}
+            }
+        }
+
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void Contains(string propertyName, string value)
+        {
+            var left = Expression.Property(Paramater, type.GetProperty(propertyName));
+            var right = Expression.Constant(value);
+            var result = Expression.Call(left, typeof(string).GetMethod("Contains", new[] { typeof(string) }), right);
+            And(result);
+        }
+
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void EndsWith(string propertyName, string value)
+        {
+            var left = Expression.Property(Paramater, type.GetProperty(propertyName));
+            var right = Expression.Constant(value);
+            var result = Expression.Call(left, typeof(string).GetMethod("EndsWith", new[] { typeof(string) }), right);
+            And(result);
+        }
+
+        /// <summary>
+        /// 判断相等
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void Equal<TValue>(string propertyName, TValue value)
+        {
+            var p = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, p);
+            var right = Expression.Constant(value, p.PropertyType);
+            var result = Expression.Equal(left, right);
+            And(result);
+        }
+        /// <summary>
+        /// 判断不相等
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void NotEqual<TValue>(string propertyName, TValue value)
+        {
+            var p = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, p);
+            var right = Expression.Constant(value, p.PropertyType);
+            var result = Expression.NotEqual(left, right);
+            And(result);
+        }
+        /// <summary>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> True()
+        {
+            return i => true;
+        }
+        /// <summary>
+        /// 获取最终的表达式
+        /// </summary>
+        /// <returns></returns>
+        public Expression<Func<T, bool>> GetExpression()
+        {
+            return filter == null ? True() : Expression.Lambda<Func<T, bool>>(filter, Paramater);
+        }
+
+        /// <summary>
+        /// 大于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void GreaterThan<TValue>(string propertyName, TValue value)
+        {
+            var prop = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+            var result = Expression.GreaterThan(left, right);
+            And(result);
+        }
+
+        /// <summary>
+        /// 大于或等于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <param name="liftToNull"></param>
+        /// <param name="method"></param>
+        public void GreaterThanOrEqual<TValue>(string propertyName, TValue value, bool liftToNull = false, MethodInfo method = null)
+        {
+            var prop = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+            var result = Expression.GreaterThanOrEqual(left, right, liftToNull, method);
+            And(result);
+        }
+
+
+        /// <summary>
+        /// 大于或等于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <param name="liftToNull"></param>
+        /// <param name="method"></param>
+        public void GreaterThanOrEqual_String<TValue>(string propertyName, TValue value, bool liftToNull = false, MethodInfo method = null)
+        {
+            var prop = type.GetProperty(propertyName);
+
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+
+            var result = Expression.GreaterThanOrEqual(Expression.Call(null, method, new Expression[2] { left, right }), Expression.Constant(0, typeof(int)));
+
+            And(result);
+        }
+
+
+
+        /// <summary>
+        /// 小于或等于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <param name="liftToNull"></param>
+        /// <param name="method"></param>
+        public void LessThanOrEqual_String<TValue>(string propertyName, TValue value, bool liftToNull = false, MethodInfo method = null)
+        {
+            var prop = type.GetProperty(propertyName);
+
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+
+            var result = Expression.LessThanOrEqual(Expression.Call(null, method, new Expression[2] { left, right }), Expression.Constant(0, typeof(int)));
+
+            And(result);
+        }
+
+
+
+        /// <summary>
+        /// 小于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void LessThan<TValue>(string propertyName, TValue value)
+        {
+            var prop = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+            var result = Expression.LessThan(left, right);
+            And(result);
+        }
+
+        /// <summary>
+        /// 小于或等于
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <param name="liftToNull"></param>
+        /// <param name="method"></param>
+        public void LessThanOrEqual<TValue>(string propertyName, TValue value, bool liftToNull = false, MethodInfo method = null)
+        {
+            var prop = type.GetProperty(propertyName);
+            var left = Expression.Property(Paramater, prop);
+            var right = Expression.Constant(value, prop.PropertyType);
+            var result = Expression.LessThanOrEqual(left, right, liftToNull, method);
+            And(result);
+        }
+
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void StartsWith(string propertyName, string value)
+        {
+            var left = Expression.Property(Paramater, type.GetProperty(propertyName));
+            var right = Expression.Constant(value);
+            var result = Expression.Call(left, typeof(string).GetMethod("StartsWith", new[] { typeof(string) }), right);
+            And(result);
+        }
+    }
+}
