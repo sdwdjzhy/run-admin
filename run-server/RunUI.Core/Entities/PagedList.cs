014920940d9c03cb8d558c9f02354c3208b94295
sdwdjzhy <sdwdjzhy@qq.com>
2021-12-16 09:32:52 +0800

    添加分页PagedList的支持

---
+++ run-server/RunUI.Core/Entities/PagedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+    public class PagedList<T>
+    {
+        /// <summary>
+        /// 总共数量
+        /// </summary>
+        public long Total { get; set; }
+        /// <summary>
+        /// 项列表
+        /// </summary>
+        public List<T> Rows { get; set; }
+        /// <summary>
+        /// 当前页，从1开始
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// 页面大小
+        /// </summary>
+        public int PageSize { get; set; }
+
+    }
+}
---
+++ run-server/RunUI.Core/Entities/PagedListResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+    public class PagedListResult<T>
+    {
+        public PagedList<T> List { get; set; }
+        public List<LabelValueGroup> Select { get; set; }
+    }
+}
