c9929fae3a4dcae1b77172393a80726c7c150d94
sdwdjzhy <sdwdjzhy@qq.com>
2021-12-16 09:33:11 +0800

    添加 ForeignEnumAttribute

---
+++ run-server/RunUI.Db.FreeSql/Attributes/ForeignEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class ForeignEnumAttribute : Attribute
+    {
+        public string EnumCode { get; set; }
+        public ForeignEnumAttribute(string enumCode)
+        {
+            EnumCode = enumCode;
+        }
+    }
+}
