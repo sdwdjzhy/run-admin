f7d2c470c186111db7619c4f0f98142f22e93631
sdwdjzhy <sdwdjzhy@qq.com>
2021-12-16 09:33:37 +0800

    添加扩展方法ToPagedListAsync

---
+++ run-server/RunUI.Db.FreeSql/Extensions/FreeSqlRepositoryExtensions.cs
@@ -0,0 +1,43 @@
+﻿using FreeSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunUI
+{
+    public static class FreeSqlRepositoryExtensions
+    {
+        public static async Task<PagedList<T>> ToPagedListAsync<T, K>(this IBaseRepository<T> repository, QueryHelper<T> qg, Expression<Func<T, K>> orderBy, bool descending = true) where T : class
+        {
+            var where = await qg.GetExpression();
+            var (page, size) = await qg.GetPageInfo();
+
+            var rows = await repository
+                .Where(where)
+                .Count(out var total)
+                .OrderByIf(true, orderBy, descending)
+                .Page(page, size)
+                .ToListAsync();
+
+            return new PagedList<T>() { Page = page, PageSize = size, Rows = rows, Total = total };
+        }
+        public static async Task<PagedList<K>> ToPagedListAsync<T, K>(this IBaseRepository<T> repository, QueryHelper<T> qg, Expression<Func<T, K>> selector, Expression<Func<T, K>> orderBy, bool descending = true) where T : class
+        {
+            var where = await qg.GetExpression();
+            var (page, size) = await qg.GetPageInfo();
+
+            var rows = await repository
+                .Where(where)
+                .Count(out var total)
+                .OrderByIf(true, orderBy, descending)
+                .Page(page, size)
+
+                .ToListAsync(selector);
+
+            return new PagedList<K>() { Page = page, PageSize = size, Rows = rows, Total = total };
+        }
+    }
+}
